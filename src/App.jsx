23245349d655c544d18e26a9a880eee3dba5d50e
devakchain <shmoelyitzhak@gmail.com>
2025-01-30 16:29:02 +0200

    git push

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AppExchange from "./components/AppExchange";
+
+function App() {
+  return (
+    <div>
+      <AppExchange />
+    </div>
+  );
+}
+
+export default App;
